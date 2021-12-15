1abc91b43400bec1c6552ab7842d9e6209c08b9b
Axlfc <axel.fernandez@mercatsdetarragona.cat>
2021-12-15 14:36:02 +0100

    UPDATED: script.py data trimmed

--- script.py
+++ script.py
@@ -2,39 +2,38 @@
 
 import pandas as pd
 
+
 df = pd.read_excel (r'Consum mobils del 2002 al 20073.xlsx')
 column_headers = df.columns.ravel()
 
 a_column = df[column_headers[0]].tolist()
 c_column = df[column_headers[2]].tolist()
 
-#unique_index = pd.Index(c_column)
 index_list = []
-c = 1
-for i in c_column:
+
+for i in a_column:
     i = str(i)
     i_list = i.split()
     for j in i_list:
-        if j == "Telefon" or j == "Telèfon" or j == "Telefón" or j == "Tèlf":
-            #print("eureka")
-            if i_list[1] == "Mobil" or i_list[1] == "Mòbil":
-                #print(i_list[2])
-                # Here we need to find the index of the current line
-                index = c_column.index(i)
-                #print("El contingut de la casella A és: " + str(a_column[index]))
+        if j != "nan" and j.isdigit():
+            index = a_column.index(i)
+            index_list.append(str(index))
 
-                print("Index del telèfon " + str(i_list[2]) + ": " + str(index + 2) + " i pertany a " + str(i_list[3:]))
-            #else:
-                # Telèfons no mòbils
-                #print(i_list[1])
-        elif j == "Telèf.Mòbil":
-            #print(i_list[1])
-            #print("holaaaa" + str(index + 2))
-            print("Index del telèfon " + str(i_list[1]) + ": " + str(index + 2))
+mobile_phone_list=[]
+for n in index_list:
+    mobile_phone_list.append(c_column[int(n)].split())
 
-    '''
-    for j in line:
-        if "Telefon" in str(j):
-            print("aleluya")'''
-    #index_list.append(unique_index.get_loc(str(i)))
-    c+=1
+for i in mobile_phone_list:
+    if i[0] == "Telefon" or i[0] == "Telèfon" or i[0] == "Telefón" or i[0] == "Tèlf":
+        tlf = str(i[1])
+        if tlf == "Mobil" or tlf == "Mòbil":
+            if len(str(i[2])) == 9 and int(str(i[2])[0]) != 9:
+                print(str(i[2]) + ":\t" + str(i[3:]))
+                print()
+        else:
+            if len(str(i[1])) == 9 and int(str(i[1])[0]) != 9:
+                print(str(i[1]) + ":\t" + str(i[2:]))
+                print()
+    elif i[0] == "Telèf.Mòbil":
+        print(str(i[1]) + ":\t" + str(i[2:]))
+        print()
